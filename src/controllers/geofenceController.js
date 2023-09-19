a0b69c41e33fc7f20f34881a76b47f2b9f6a21c4
mfarizalpasha <mfarizalpasha@gmail.com>
2023-09-19 20:22:18 +0700

    update

--- src/controllers/geofenceController.js
+++ src/controllers/geofenceController.js
@@ -1,49 +1,51 @@
-// controllers/geofenceController.js
 const Geofence = require("../models/geofence");
 
-// Menyimpan data Geofence ke dalam database
-exports.saveGeofenceData = (req, res) => {
-  const { username, latitude, longitude, radius, start_time, end_time } =
-    req.body;
+// Controller untuk menyimpan data Geofence ke dalam database
+async function saveGeofenceData(req, res) {
+  try {
+    const { username, latitude, longitude, radius, start_time, end_time } =
+      req.body;
 
-  if (
-    !username ||
-    !latitude ||
-    !longitude ||
-    !radius ||
-    !start_time ||
-    !end_time
-  ) {
-    return res.status(400).json({ error: "Semua kolom harus diisi" });
+    if (
+      !username ||
+      !latitude ||
+      !longitude ||
+      !radius ||
+      !start_time ||
+      !end_time
+    ) {
+      return res.status(400).json({ error: "Semua kolom harus diisi" });
+    }
+
+    const newGeofence = new Geofence({
+      username,
+      latitude,
+      longitude,
+      radius,
+      start_time,
+      end_time,
+    });
+
+    await newGeofence.save();
+    res.status(201).json({ message: "Data Geofence berhasil disimpan" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Gagal menyimpan data Geofence" });
   }
+}
 
-  const newGeofence = new Geofence({
-    username,
-    latitude,
-    longitude,
-    radius,
-    start_time,
-    end_time,
-  });
+// Controller untuk mengambil semua data Geofence dari database
+async function getGeofenceData(req, res) {
+  try {
+    const geofences = await Geofence.find({});
+    res.status(200).json(geofences);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Gagal mengambil data Geofence" });
+  }
+}
 
-  newGeofence.save((err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: "Gagal menyimpan data Geofence" });
-    } else {
-      res.status(201).json({ message: "Data Geofence berhasil disimpan" });
-    }
-  });
-};
-
-// Mengambil semua data Geofence dari database
-exports.getGeofenceData = (req, res) => {
-  Geofence.find({}, (err, geofences) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: "Gagal mengambil data Geofence" });
-    } else {
-      res.status(200).json(geofences);
-    }
-  });
+module.exports = {
+  saveGeofenceData,
+  getGeofenceData,
 };
--- src/routes/geofenceRoutes.js
+++ src/routes/geofenceRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const geofenceController = require("../controllers/geofenceController");
 
 // Rute untuk menyimpan data Geofence ke dalam database
-router.post("/save_geofence_data", geofenceController.saveGeofenceData);
+router.post("/data", geofenceController.saveGeofenceData);
 
 // Rute untuk mengambil semua data Geofence dari database
-router.get("/get_geofence_data", geofenceController.getGeofenceData);
+router.get("/data", geofenceController.getGeofenceData);
 
 module.exports = router;
