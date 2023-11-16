3e31c855f7c02da15790231c893c1952e2da5a0e
mfarizalpasha <mfarizalpasha@gmail.com>
2023-11-17 03:59:14 +0700

    aa

--- src/routes/geofenceHistoryRoutes.js
+++ src/routes/geofenceHistoryRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const geofenceHistoryController = require("../controllers/geofenceHistoryController");
-// const { authenticateToken } = require("../middleware/authMiddleware");
 
 router.post("/data", geofenceHistoryController.saveGeofenceHistoryData);
 
--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const notificationController = require("../controllers/notificationController");
-// const { authenticateToken } = require("../middleware/authMiddleware");
 
 router.post("/data", notificationController.saveNotificationData);
 
